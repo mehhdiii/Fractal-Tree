4cd8f4cae6f50f7ad3262b4fa79c622d583bd558
mk <mk05198@st.habib.edu.pk>
2021-05-21 17:02:55 +0500

    flush implimented

--- rw.py
+++ rw.py
@@ -1,7 +1,26 @@
+import pickle
+
 class rw:
     # A class which reads and writes data to the hardisk
     
     def __init__(self) -> None:
         pass
+
+    def read(self, dir: str, filename):
+        with open(filename) as file:
+            data = pickle.load(file)
+
+        return data
+
+
+    def write(self, dir: str, filename, data):
+        with open(filename, 'wb') as file:
+            pickle.dump(data, file)
+myobj = rw()
+        
+
+    
+
+
 
     
