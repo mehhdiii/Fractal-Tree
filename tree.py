17d40155fee75654546593666e1b0d2bb3a2dd19
mk <mk05198@st.habib.edu.pk>
2021-05-22 05:08:30 +0500

    tree implimented with some errors

--- tree.py
+++ tree.py
@@ -1,45 +1,3 @@
-# import math
-# class Tree:
-#     def __init__(self): 
-#         pass
-
-#     def search(self, item): 
-#         pass
-
-#     def insert(self, item): 
-#         pass
-
-#     def remove(self, item): 
-#         pass
-    
-#     def is_empty(self): 
-#         pass
-
-#     def size(self): 
-#         pass
-
-# class node: 
-#     """
-#         Each node stores key value pair (not unique). In case of same keys, the values are stored as list
-
-#         Attributes: 
-#         order := Max number of keys in the node.
-#         leaf := True if the node is a leaf node
-#         Buffer_size := size of the Buffer 
-#     """
-#     def __init__(self, order):
-#         self.order = order
-#         self.BUFFER_SIZE = math.sqrt(order)
-#         self.keys = []
-#         self.values = []
-#         self.leaf = False
-    
-#     def add()
-
-
-
-
-
 
 class Node(object):
     """Base node object.
@@ -53,86 +11,47 @@ class Node(object):
         """Child nodes can be converted into parent nodes by setting self.leaf = False. Parent nodes
         simply act as a medium to traverse the tree."""
         print("Node created")
+
+        # Tree variables
         self.order = order
-        self.buffer = order # size needs to be corrected later
-        self.messages = []
+        self.max_children = order
+        self.max_keys = order-1 
+        self.min_keys = order//2 -1 
+
+        #data variable
         self.keys = []
         self.values = []
         self.leaf = True
-        self.children = []
+        
+        #pointers
+        self.parent = None
+        self.children = [None]*self.order
+
+        #Fractal tree index specific variables
+        self.buffer = order//2 # size needs to be corrected later
+        self.messages = []
+
+    def update_leaf(self):
+        """
+        updates the leaf status of a node 
+        """
+        if [None]*self.order == self.children:
+            self.leaf = False
 
 
-    def add(self, key, value):
-        """Adds a key-value pair to the node."""
-        # If the node is empty, simply insert the key-value pair.
-        if not self.keys:
-            self.keys.append(key)
-            self.values.append([value])
-            return None
-
-        for i, item in enumerate(self.keys):
-            # If new key matches existing key, add to list of values.
-            if key == item:
-                self.values[i].append(value)
-                break
-
-            # If new key is smaller than existing key, insert new key to the left of existing key.
-            elif key < item:
-                self.keys = self.keys[:i] + [key] + self.keys[i:]
-                self.values = self.values[:i] + [[value]] + self.values[i:]
-                break
-
-            # If new key is larger than all existing keys, insert new key to the right of all
-            # existing keys.
-            elif i + 1 == len(self.keys):
-                self.keys.append(key)
-                self.values.append([value])
-
-    def split(self):
-        """Splits the node into two and stores them as child nodes."""
-        left = Node(self.order)
-        right = Node(self.order)
-        mid = self.order // 2
-
-        left.keys = self.keys[:mid]
-        left.values = self.values[:mid]
-
-        right.keys = self.keys[mid:]
-        right.values = self.values[mid:]
-
-        # When the node is split, set the parent key to the left-most key of the right child node.
-        self.keys = [right.keys[0]]
-        self.values = [left, right]
-        self.leaf = False
 
     def is_full(self):
         """Returns True if the node is full."""
         return len(self.keys) == self.order
 
-    def show(self, counter=0):
-        """Prints the keys at each level."""
-        print(counter, str(self.keys))
 
-        # Recursively print the key of child nodes (if these exist).
-        if not self.leaf:
-            for item in self.values:
-                item.show(counter + 1)
 
 class FractalTree(object):
-    """B+ tree object, consisting of nodes.
-    Nodes will automatically be split into two once it is full. When a split occurs, a key will
-    'float' upwards and be inserted into the parent node to act as a pivot.
-    Attributes:
-        order (int): The maximum number of keys each node can hold.
-    """
+
+
     def __init__(self, order=8):
         self.root = Node(order)
         print("Tree created!")
-
-
-
-    def print_children(self):
-        print(self.root.children)
 
 
 
@@ -140,33 +59,133 @@ class FractalTree(object):
         #### finds the child where key values should move down to  
         pass 
 
-        # """ For a given node and key, returns the index where the key should be inserted and the
-        # list of values at that index."""
-        # for i, item in enumerate(node.keys):
-        #     if key < item:
-        #         return node.values[i], i
 
-        # return node.values[i + 1], i + 1
 
-    def _merge(self, parent, child, index):
-        """For a parent and child node, extract a pivot from the child to be inserted into the keys
-        of the parent. Insert the values from the child into the values of the parent.
+
+    def add(self, node: Node, key, value): 
+        """Adds a key-value pair to the passed node. if node is full, split and perform the necessary steps.
+        returns the index at which the value was inserted. 
         """
-        parent.values.pop(index)
-        pivot = child.keys[0]
 
-        for i, item in enumerate(parent.keys):
-            if pivot < item:
-                parent.keys = parent.keys[:i] + [pivot] + parent.keys[i:]
-                parent.values = parent.values[:i] + child.values + parent.values[i:]
-                break
+        #check if tree is empty: 
+        if (self.root.keys == []):
+            node.keys.append(key)
+            node.values.append(value)
 
-            elif i + 1 == len(parent.keys):
-                parent.keys += [pivot]
-                parent.values += child.values
-                break
 
-    def insert(self, key, value):
+        else:
+
+            #search for its place in the node: 
+            for indx, k in enumerate(node.keys):
+                if (k > key):
+                    #insert at this position:
+                    node.keys = node.keys[:indx]+[key] + node.keys[indx:]
+                    if (node.leaf):
+                        node.values = node.values[:indx] + [value] + node.values[indx:]
+                    break
+                #check if key is even greater than last element: 
+                if (indx == len(node.keys)-1 and key >= k ):
+                    #add the key to the end of list
+                    node.keys.append(key)
+                    if (node.leaf):
+                        node.values.append(value)
+                    break
+
+            
+        #now check if the node's space has exceeded
+        if len(node.keys) > node.max_keys:
+            self.split(node)
+
+         
+
+
+
+    def split(self, node: Node):
+
+        """Splits the node into two and stores them as child nodes."""
+
+        center_node_index = node.order//2
+        center_key, center_value = node.keys[center_node_index], node.values[center_node_index]
+
+        left = Node(node.order)
+        right = Node(node.order)
+
+        #copying data:
+
+        #copy children: 
+        left.children = node.children[:center_node_index+1]
+        right.children = node.children[center_node_index+1:]
+        
+        #update thier leaf status
+        left.update_leaf()
+        right.update_leaf()
+
+        
+        #if the node is leaf node, then copy center_node_index to the right_node as well: 
+        if(node.leaf): 
+            ## add the center key value pair as well into the left and right child along with the right half of it. 
+            right.keys, right.values = node.keys[center_node_index+1:], node.values[center_node_index+1:]
+            left.keys, left.values = node.keys[:center_node_index], node.values[:center_node_index]
+        
+        else:
+            right.keys, right.values = node.keys[center_node_index:]
+            left.keys, left.values = node.keys[:center_node_index]
+
+        
+
+        
+        if (node.parent==None): #if the node doesnt have any parent, it means we are at root node
+
+
+            #make a new node from this center key: 
+            new_parent_node = Node(node.order)
+            #add the center key to this parent node: 
+            new_parent_node.keys.append(center_key) 
+
+            # make left, right the child of new_parent_node: 
+            new_parent_node.children[0] = left
+            new_parent_node.children[1] = right
+            
+            # update leaf status: 
+            new_parent_node.update_leaf()
+            
+            #make this node the new root node: 
+            self.root = new_parent_node 
+            
+            #add reference to parent 
+            left.parent, right.parent = self.root, self.root
+
+
+        
+        
+        elif (node.parent!=None): #if the node has a parent
+            #now update the parents children to include left and right nodes 
+            parents_children = node.parent.children
+            
+            #search for node in parents_children
+            for indx, n in enumerate(parents_children):
+                if (n==node):
+                    ##add left and right to indx and indx+1 location in children list 
+                    node.parent.children[indx] = left
+
+                    try:
+                        node.parent.children[indx+1] = right
+                    except:
+                        node.parent.children.append(right)
+
+
+            #add the element to its parent node
+            self.add(node.parent, center_key, center_value) 
+
+            
+
+
+    def insert_msg(self, key, value):
+        """
+        Inserts a message to insert the key value pair in the tree.
+        
+        """
+
         # if buffer is not full, add the msg to buffer
         if(self.root.buffer != len(self.root.messages)):
             print((key, value), ":=", "msg added to buffer!")
@@ -179,31 +198,6 @@ class FractalTree(object):
             self.root.messages.append(("insert", key, value))
 
 
-            
-
-
-
-        # """Inserts a key-value pair after traversing to a leaf node. If the leaf node is full, split
-        # the leaf node into two.
-        # """
-        # parent = None
-        # child = self.root
-
-        # # Traverse tree until leaf node is reached.
-        # while not child.leaf:
-        #     parent = child
-        #     child, index = self._find(child, key)
-
-        # child.add(key, value)
-
-        # # If the leaf node is full, split the leaf node into two.
-        # if child.is_full():
-        #     child.split()
-
-        #     # Once a leaf node is split, it consists of a internal node and two leaf nodes. These
-        #     # need to be re-inserted back into the tree.
-        #     if parent and not parent.is_full():
-        #         self._merge(parent, child, index)
 
     def flush(self, node: Node):
         ## flush msgs from node down a level:
@@ -248,93 +242,37 @@ class FractalTree(object):
             if command =="insert":
                 key, value = msg[1], msg[2]
                 ## insertion code goes here
+                self.add(node, key, value)
 
 
 
+    # def retrieve(self, key):
+    #     """Returns a value for a given key, and None if the key does not exist."""
+    #     child = self.root
+
+    #     while not child.leaf:
+    #         child, index = self._find(child, key)
+
+    #     for i, item in enumerate(child.keys):
+    #         if key == item:
+    #             return child.values[i]
+
+    #     return None
 
 
-
-
-
-    def retrieve(self, key):
-        """Returns a value for a given key, and None if the key does not exist."""
-        child = self.root
-
-        while not child.leaf:
-            child, index = self._find(child, key)
-
-        for i, item in enumerate(child.keys):
-            if key == item:
-                return child.values[i]
-
-        return None
-
-    def show(self):
-        """Prints the keys at each level."""
-        self.root.show()
-
-# def demo_node():
-#     print('Initializing node...')
-#     node = Node(order=4)
-
-#     print('\nInserting key a...')
-#     node.add('a', 'alpha')
-#     print('Is node full?', node.is_full())
-#     node.show()
-
-#     print('\nInserting keys b, c, d...')
-#     node.add('b', 'bravo')
-#     node.add('c', 'charlie')
-#     node.add('d', 'delta')
-#     print('Is node full?', node.is_full())
-#     node.show()
-
-#     print('\nSplitting node...')
-#     node.split()
-#     node.show()
-
-# def demo_bplustree():
-#     print('Initializing B+ tree...')
-#     bplustree = BPlusTree(order=4)
-
-#     print('\nB+ tree with 1 item...')
-#     bplustree.insert('a', 'alpha')
-#     bplustree.show()
-
-#     print('\nB+ tree with 2 items...')
-#     bplustree.insert('b', 'bravo')
-#     bplustree.show()
-
-#     print('\nB+ tree with 3 items...')
-#     bplustree.insert('c', 'charlie')
-#     bplustree.show()
-
-#     print('\nB+ tree with 4 items...')
-#     bplustree.insert('d', 'delta')
-#     bplustree.show()
-
-#     print('\nB+ tree with 5 items...')
-#     bplustree.insert('e', 'echo')
-#     bplustree.show()
-
-#     print('\nB+ tree with 6 items...')
-#     bplustree.insert('f', 'foxtrot')
-#     bplustree.show()
-
-#     print('\nRetrieving values with key e...')
-#     print(bplustree.retrieve('e'))
-
-# if __name__ == '__main__':
-#     demo_node()
-#     print('\n')
-#     demo_bplustree()
     
 mytree = FractalTree(4)
-mytree.print_children()
-mytree.insert(2, "panda")
-mytree.insert(3, "panda")
-mytree.insert(4, "panda")
-mytree.insert(5, "panda")
-mytree.insert(6, "panda")
-print(mytree.root.messages)
+# mytree.print_children()
+mytree.add(mytree.root, 1, "1")
+mytree.add(mytree.root, 4, "4")
+mytree.add(mytree.root, 7, "7")
+mytree.add(mytree.root, 10,"10")
+mytree.add(mytree.root.children[1], 17,"17")
+mytree.add(mytree.root.children[1], 21,"21")
+mytree.add(mytree.root.children[2], 31,"31")
+mytree.add(mytree.root.children[2], 25,"25")
+mytree.add(mytree.root.children[2], 19,"19")
+mytree.add(mytree.root.children[2], 20,"20")
+# mytree.add(6, "panda")
+print(mytree.root.values)
     
