338690ac0e1e437a5f34f03f1820ccb931ee306f
ii-06178 <60180644+ii-06178@users.noreply.github.com>
2021-05-26 23:36:29 +0500

    cleaning query section

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
 import random
 from FractalTree import FractalTree
 import query
-bplustree = FractalTree(order=8)
-# for i in range(1000000):
-#     x = random.randint(1, 1000)
-#     bplustree.buffer((x, str(i)))
-
-bplustree.buffer((1,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer(( 4,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree = FractalTree(order=8)  # fractal tree object
+# This is the dummmy data for the Fractal Tree where it takes a key, a dictionary for a value and the operation that needs to be done on it
+bplustree.buffer((1, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((4, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
 bplustree.buffer((7, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((10,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((17,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((21,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((31,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((25,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((19,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((20,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((28,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((42,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((15,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
-bplustree.buffer((41,{"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((10, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((17, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((21, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((31, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((25, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((19, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((20, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((28, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((42, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((15, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
+bplustree.buffer((41, {"Name": "Ifrah Ilyas", "Age": "20"}, "insert"))
 
-# bplustree.buffer((5,"5", "insert"))
-# bplustree.buffer((3,"3", "insert"))
-# bplustree.buffer((2,"2", "insert"))
 
-# bplustree.buffer((100,"5", "insert"))
-# bplustree.buffer((-2,"3", "insert"))
-# bplustree.buffer((-200,"2", "insert"))
-# bplustree.buffer((-100,"5", "insert"))
-# bplustree.buffer((97,"3", "insert"))
-# bplustree.buffer((-96,"2", "insert"))
-bplustree.show()
-
-print(bplustree.search_retrieval(-96))
-# bplustree.calling_ra()
-# bplustree.all_buffer_flush(bplustree.root)
-q=query.Query(bplustree)
-# q.user_input()
-q.show()
+q = query.Query(bplustree)  # query class
+q.user_input()  # takes user input
+q.show()  # this shows the data we have put as input as table
--- query.py
+++ query.py
@@ -6,13 +6,11 @@ class Query():
         self.tree=t
 
     def isvalid_input(self,str):
+        # this checks if the input provided is in valid syntax
+        # function is only true when there is valid syntax or input is "Q" that is quit
         lst = str.split()
         if lst[0].lower() in ['insert','delete'] and lst[1].isnumeric():
             return True
-        
-        # if len(lst) == 3 and lst[0].lower() in ['insert','delete'] and lst[1].isnumeric():
-            
-        #     return True
         elif str == "Q":
             return True
         elif not lst[1].isnumeric():
@@ -26,6 +24,7 @@ class Query():
             return False
 
     def user_input(self):
+        # this takes in the input from the user until the user quits by writing Q
         print()
         print(" - Command format: <insert/delete> <key> <value>")
         print(" - Enter 'Q' to quit")
@@ -37,7 +36,7 @@ class Query():
             if str != "Q" and self.isvalid_input(str):
                 command=str.split()
                 if '{' in str:
-                    ins=str[str.find('{'):]
+                    ins=str[str.find('{'):] # this is for taking value which is in the form of a dictionary
                     print(ins)
                     command=command[:2]
                     command.append(json.loads(ins))
@@ -49,42 +48,31 @@ class Query():
                 continue
 
     def show(self):
+        # this function shows the data in the form of a table
         print()
-        dict=self.tree.calling_ra()
+        dict=self.tree.calling_ra() # taking data from Fractal Trees
         print(dict)
         keys=[]
         values=[]
         print()
         print("Displaying Tree: ")
         for k,v in dict.items():
-            keys.append(k)
+            keys.append(k) # making separate lists for keys and valued
             for i in v:
                     if type(i)!=str:
                         values.append(i)
-        print(keys,values)
-        print()
         set={}
         for i in values:
-            for j,v in values[0].items():
+            for j,v in values[0].items(): # this makes a dictionary where the the index of the values in the array correspond to that of in the key array
                 if j not in set.keys():
                     set[j]=[]
                     set[j].append(i.get(j))
                 else:
                     set[j].append(i.get(j))
         print(set)
-        df=pandas.DataFrame(set,index=keys)
+        df=pandas.DataFrame(set,index=keys) # makes a 'dataframe' or table for the data
         print(df)
         return
 
 
 
-# def apply_msg(self, node):
-#         """Applies the insert/delete message on the -> node"""
-#         for msg in node.buffer:
-#             command, key, value = msg
-#             if command.lower() == "insert":
-#                 key, value = int(msg[1]), msg[2]
-#                 ## insertion code goes here
-#                 self.insert(node.parent,node, key, value)
-#         #now clear the buffer: 
-#         node.buffer = []
